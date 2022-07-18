478dd00a74c294e4137b17bf6f4805f6bf177ef1
miirlo11 <miirlo11@gmail.com>
2022-07-18 12:11:13 -0600

    Finish Saml

--- saml_backend/saml_authentication/views.py
+++ saml_backend/saml_authentication/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from curses.ascii import HT
+import requests
 
-# Create your views here.
+
--- saml_backend/saml_backend/urls.py
+++ saml_backend/saml_backend/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import django_saml2_auth.views
+from saml_authentication import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     # These are the SAML2 related URLs. You can change "^saml2_auth/" regex to
 # any path you want, like "^sso_auth/", "^sso_login/", etc. (required)
     path('saml2_auth/', include('django_saml2_auth.urls')),
@@ -33,4 +33,7 @@ urlpatterns = [
     # If you want to specific the after-login-redirect-URL, use parameter "?next=/the/path/you/want"
     # with this view.
     path('admin/login/', django_saml2_auth.views.signin),
+    
+    path('admin/', admin.site.urls),
+    
 ]
